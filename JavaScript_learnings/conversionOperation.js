4926b60be4ef77cc8d3062813d9cc43144ed53e9
parmardipika <parmardipika61825@gmail.com>
2025-05-30 23:03:25 +0530

    conversions & operations done

---
+++ JavaScript_learnings/conversionOperation.js
@@ -0,0 +1,92 @@
+// let score="33"
+// console.log(typeof score )
+// let valueinNumber=Number(score)
+// console.log(typeof valueinNumber )
+
+// score="33abc"
+// console.log(typeof score )
+// console.log(typeof(score))
+// valueinNumber=Number(score)
+// console.log(typeof valueinNumber )
+// console.log(valueinNumber )//not a number since: it is 33abc not any num like 33
+
+
+// score=null
+// console.log(typeof score )
+// valueinNumber=Number(score)
+// console.log(typeof valueinNumber )
+// console.log(valueinNumber )//not a number since: it is 33abc not any num like 33
+
+// score=undefined
+// console.log(typeof score )
+// valueinNumber=Number(score)
+// console.log(typeof valueinNumber )
+// console.log(valueinNumber )//not a number since: it is 33abc not any num like 33
+
+// score=true
+// console.log(typeof score )
+// valueinNumber=Number(score)
+// console.log(typeof valueinNumber )
+// console.log(valueinNumber )//not a number since: it is 33abc not any num like 33
+
+// score="yoyo"
+// console.log(typeof score )
+// valueinNumber=Number(score)
+// console.log(typeof valueinNumber )
+// console.log(valueinNumber )//not a number since: it is 33abc not any num like 33
+
+
+// //"33"=> 33
+// //"33abc"=>Nan = Not a Number
+// //true=> 1; false => 0
+
+// let islog=1
+// let booleanislog=Boolean(islog)
+// console.log(booleanislog);
+
+// //1=> true; 0=> false
+// //"" => false
+// //"yoyo" => true
+
+// let num=33
+// let stringnum=String(num)
+// console.log(stringnum);
+// console.log(typeof stringnum);
+
+// ********************Operations******************
+
+let val=3
+let negval= -val
+console.log(negval);
+
+//operations:
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2/2);
+
+// let str1= "hihi"
+// let str2= " yoyo"
+
+// let str3= str1 + str2
+// console.log(str3)
+
+// console.log("1"+2); //ans: 12
+// console.log(1+"2");//ans:12
+// console.log("1"+2+2);//ans:122
+// console.log(1+2+"2");///ans:32
+
+//tricky:
+console.log(+true); //ans:1  /automatic conversion using +
+console.log(+""); //ans: 0
+
+let num1,num2,num3
+num1=num2=num3=2+2 
+
+let gameCounter=100
+gameCounter++; //or ++gameCounter
+console.log(gameCounter)  //ans:101
+
