4ea24534333b9b6466f32027b5cb57f0d3089750
parmardipika <parmardipika61825@gmail.com>
2025-05-30 23:03:58 +0530

    Data_types done

---
+++ JavaScript_learnings/data_types.js
@@ -0,0 +1,27 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3) //we r using nodejs,not browser
+
+console.log(3
+    +3) //code readability should be high
+
+console.log("yoyo") 
+
+let name="yoyo"  //string
+let age=273  //numbers
+let islogged=false //boolean
+
+//number==> range: 2 to power 53
+//bigint
+//string=> "" me
+//boolean=> true or false
+//null=> standaline value (empty value)
+//undefined=> abhi val assign nhi hui
+//symbol=> unique
+
+//object
+
+console.log(typeof 23);
+console.log(typeof null); //ans: object
+console.log(typeof undefined);//ans: undefined becoz type hai in java
+
