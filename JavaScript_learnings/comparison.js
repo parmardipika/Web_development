2d45bfa87dedae7347d14e2371f0b67ba86ed2a9
parmardipika <parmardipika61825@gmail.com>
2025-05-31 19:57:42 +0530

    comparison oper. done

---
+++ JavaScript_learnings/comparison.js
@@ -0,0 +1,27 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2" >1) //and: true automatic conversion of "2" => 2
+console.log("02" >1)//similarly for this.
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+//i js, for ==, it checkhs directly but for comarison operators
+//;ike > < >= <= , it converts null to number treating it to 
+//zero(0), thats why null>0 is false & null>=0 is true
+
+//undefined ke saath sb false hote
+
+
+// ===  : ye data type+val check krega
+console.log("2"===2)  //ans:false
+
+
+
+
+
