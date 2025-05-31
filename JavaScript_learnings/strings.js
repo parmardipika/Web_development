58696336ae3be16f8544d47ee19a2e66db4c5299
parmardipika <parmardipika61825@gmail.com>
2025-05-31 19:58:21 +0530

    Strings intro done

---
+++ JavaScript_learnings/strings.js
@@ -0,0 +1,23 @@
+const name="hitesh"
+const repo= 50
+
+console.log(name+repo+"val"); //not a good method
+
+console.log(`Hello this is ${name} and my rep is ${repo} `); //good one
+ 
+//this://
+const namee="hitesh"
+//or other method:
+const gameNum=new String('hiteshhc')
+console.log(gameNum[0]); //key access
+console.log(gameNum.__proto__);  //ans: {} =>prototype access => obj
+console.log(gameNum.length);
+//.charAt()
+//.indexOf()
+//.substring(0,n)=> n not included//isme ive ignore krke 0 se suru krega
+//.slice(_,_)=> (-3,9): last se suru krega
+//.trim() => start & end space remove
+//.replace(search, replacewith) =>
+//.includes(" ")=>? true oe false 
+//.split(separater ex: space)   => to array based on something like spaces 
+//
